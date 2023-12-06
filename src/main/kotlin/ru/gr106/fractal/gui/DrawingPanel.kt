ec47be2f19574ba4c3b8f25f734ed2c40f90f9e2
greebear567 <nailkhakimov123@mail.ru>
2023-12-06 17:42:33 +0300

    сделал плавный сдвиг, починил отмену действий

--- src/main/kotlin/ru/gr106/fractal/gui/DrawingPanel.kt
+++ src/main/kotlin/ru/gr106/fractal/gui/DrawingPanel.kt
@@ -27,7 +27,13 @@ class DrawingPanel(val p:Painter) : JPanel() {
         this.addMouseListener(object : MouseAdapter(){
             override fun mousePressed(e: MouseEvent?) {
                 if(e?.button==1) {
+
+                    rect.addPoint(0, 0)
+                    rect.addPoint(width, height)
+                    selectedListener.get(1).let{it(rect)}//запоминает состояние фрактала
+
                     mouseButtonPressed = 1
+
                     e.let {
                         rect = SelectionRect().apply {
                             addPoint(it.x, it.y)
@@ -39,19 +45,23 @@ class DrawingPanel(val p:Painter) : JPanel() {
                         }
                     }
                 } else if(e?.button==3){
+
                     mouseButtonPressed = 3                              
                     mouseButtonStartPointEndPoint.add(e.x to e.y)
+
                     rect.addPoint(0, 0)
+                    rect.addPoint(width, height)
+                    selectedListener.get(1).let{it(rect)}//запоминает состояние фрактала
                 }
             }
 
             override fun mouseReleased(e: MouseEvent?) {
                 if (mouseButtonPressed == 1) {
                     e?.let {
-                        mouseButtonPressed = -1
+
                         if (rect.isCreated) drawRect()
                         rect.addPoint(it.x, it.y)
-                        selectedListener.forEach { it(rect) }
+                        selectedListener.get(0).let{ it(rect) }
 
                         rect.resetPoints() //обнуляем координаты точек, тк они не обнуляются
                         mouseButtonPressed = -1
@@ -59,15 +69,13 @@ class DrawingPanel(val p:Painter) : JPanel() {
                 } else if (mouseButtonPressed == 3){
                     e?.let {
                         mouseButtonStartPointEndPoint.add(e.x to e.y)
-                        rect.addPoint(width, height)
 
                         rect.difX =
                             mouseButtonStartPointEndPoint.get(1).first.minus(mouseButtonStartPointEndPoint.get(0).first)
                         rect.difY =
                             mouseButtonStartPointEndPoint.get(1).second.minus(mouseButtonStartPointEndPoint.get(0).second)
 
-                        //selectedListener.forEach { it(rect) } //двигаем сам экран
-                        selectedListener.get(0).let{it(rect)}
+                        selectedListener.get(0).let{it(rect)}//двигаем экран
 
                         mouseButtonPressed = -1
 
@@ -91,18 +99,13 @@ class DrawingPanel(val p:Painter) : JPanel() {
                 } else if(mouseButtonPressed == 3){
                     e?.let {
                         mouseButtonStartPointEndPoint.add(e.x to e.y)
-                        rect.addPoint(width, height)
 
-                        rect.difX =
-                            mouseButtonStartPointEndPoint.get(1).first.minus(mouseButtonStartPointEndPoint.get(0).first)
-                        rect.difY =
-                            mouseButtonStartPointEndPoint.get(1).second.minus(mouseButtonStartPointEndPoint.get(0).second)
+                        rect.difX = mouseButtonStartPointEndPoint.get(1).first.minus(mouseButtonStartPointEndPoint.get(0).first)
+                        rect.difY = mouseButtonStartPointEndPoint.get(1).second.minus(mouseButtonStartPointEndPoint.get(0).second)
 
-                        //selectedListener.forEach { it(rect) } //двигаем сам экран
-                        selectedListener.get(1).let{it(rect)}
+                        selectedListener.get(0).let{it(rect)} //двигаем экран
 
-
-                        mouseButtonStartPointEndPoint.removeAt(0) //чистим наш список, чтобы при следующем двигании у нас был новый вектор движения экрана
+                        mouseButtonStartPointEndPoint.removeAt(0) //убираем первую точку, чтобы в списке остался 1 элемент
                     }
                 }
             }
